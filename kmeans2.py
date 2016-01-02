ec285a27211c794749046f02f39cb766e8dc90e9
Ryan Kuhle <ryan@ryankuhle.com>
2016-01-01 19:29:51 -0600

    Count countries in data set

--- kmeans2.py
+++ kmeans2.py
@@ -2,14 +2,18 @@ import pandas as pd
 
 un_data = pd.read_csv('un.csv')
 
-#Determine how many rows are in the dataset.
-#print "There are %s rows in the United Nations dataset." % un_data.shape[0]
+# Determine how many rows are in the dataset.
+# print "There are %s rows in the United Nations dataset." % un_data.shape[0]
 
-#Determine the number of non-null values present in each column.
-print un_data.count()
+# Determine the number of non-null values present in each column.
+# print un_data.count()
 
+# Which columns do you think will be the best to try to cluster on?
+# Columns to use = lifeMale, lifeFemale, infantMortality, GDPperCapita
 
-#Based on the number of non-null values, which columns do you think will be the best to try to cluster on?
 #Determine the data type of each column.
 #How many countries are present in the dataset?
+print "There are %s countries United Nations dataset." % \
+       un_data['country'].count()
+
 #We're going to see how lifeMale, lifeFemale and infantMortality cluster according to GDPperCapita, keeping in mind that we don't know in advance how many clusters there will be.
